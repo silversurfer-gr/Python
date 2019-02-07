5c4acdf75b83de00a0e60268a243f1875353a75b
silversurfer-gr <44963242+silversurfer-gr@users.noreply.github.com>
2019-02-07 13:03:33 +0100

    Add stuff

--- BlinkLED.py
+++ BlinkLED.py
@@ -1,20 +1,35 @@
 import RPi.GPIO as GPIO
 import time
-ledPin = 11    # RPI Board pin11
+
+ledPin = 5    # RPI Board pin17
+dauer  = 5
+
 def setup():
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(ledPin, GPIO.OUT)
-GPIO.output(ledPin, GPIO.LOW) # Set ledPin low to off led print ('using pin%d'%ledPin)
-# Numbers GPIOs by physical location # Set ledPin's mode is output
+    print("setup ist entert")
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(ledPin, GPIO.OUT)
+    GPIO.output(ledPin, GPIO.LOW) # Set ledPin low to off led print ('using pin%d'%ledPin)
+        # Numbers GPIOs by physical location # Set ledPin's mode is output
+
 def loop():
+    print("loop is entert")
     while True:
-GPIO.output(ledPin, GPIO.HIGH) # led on print ('...led on')
-time.sleep(1)
-GPIO.output(ledPin, GPIO.LOW) # led off print ('led off...')
-        time.sleep(1)
+        print("is looping LED-Pin: "+str(ledPin))
+        GPIO.output(ledPin, GPIO.HIGH) # led on print ('...led on')
+        print("LED on")
+        time.sleep(dauer)
+        GPIO.output(ledPin, GPIO.LOW) # led off print ('led off...')
+        print("LED off")
+        time.sleep(dauer)
+
 def destroy():
-GPIO.output(ledPin, GPIO.LOW) # led off GPIO.cleanup() # Release resource
-if __name__ == '__main__': # Program start from here setup()
-try: loop()
-except KeyboardInterrupt: # When 'Ctrl+C' is pressed, the child program destroy() will be executed.
-destroy()
+    print("destroy is entert")
+    GPIO.output(ledPin, GPIO.LOW) # led off
+    GPIO.cleanup() # Release resource
+
+if __name__ == '__main__': # Program start from here
+    setup()
+    try:
+        loop()
+    except KeyboardInterrupt: # When 'Ctrl+C' is pressed, the child program destroy() will be executed.
+        destroy()
